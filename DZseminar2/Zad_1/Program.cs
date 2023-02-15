b6f35c95d42b59577558e33285ad2c3628d26dc0
Alisa Dhelomanova <89995351029@mail.ru>
2023-02-15 23:12:20 +0300

    решение с методом

--- DZseminar2/Zad_1/Program.cs
+++ DZseminar2/Zad_1/Program.cs
@@ -3,5 +3,17 @@
 //вторую цифру этого числа.
 Console.WriteLine("Введите трехзначное число ");
 int value = int.Parse(Console.ReadLine());
-int b = (value % 100)/10;
-System.Console.WriteLine(b);
+int b;
+if((value >= 100) && (value <= 999))
+{
+   Program10(value, out b);
+   Console.WriteLine(b);
+}
+else
+{
+   Console.WriteLine("Ошибка, введите трехзначное число"); 
+}
+static void Program10(int a, out int result)
+{
+result = (a % 100)/10; 
+}
