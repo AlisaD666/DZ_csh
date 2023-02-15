9dc3a2afe6388770a6738d9157af6c7ed6145301
Alisa Dhelomanova <89995351029@mail.ru>
2023-02-15 11:11:48 +0300

    задача не совсем решена

---
+++ DZseminar2/Zad_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+int thirdNumber = 0;
+while(number > 999)
+{
+    number = number / 10;
+}
+thirdNumber = number % 10;
+Console.WriteLine(thirdNumber);
+
+if(number / 100 == 0)
+{
+  Console.WriteLine("Третьей цифры нет");  
+}
