98ae58ba9ba292762b2cb5f3ca592b2eec6440ee
Alisa Dhelomanova <89995351029@mail.ru>
2023-02-15 11:28:21 +0300

    задача с днями неделями

---
+++ DZseminar2/Zad_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите номер дня недели ");
+int numberDay = int.Parse(Console.ReadLine());
+if(numberDay > 5)
+{
+    Console.WriteLine("Выходной!");
+}
+else
+{
+   Console.WriteLine("Рабочий день"); 
+} 
