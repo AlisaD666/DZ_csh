ba09af01cd47bee1306df1451ffd5ba52f9de8e0
Alisa Dhelomanova <89995351029@mail.ru>
2023-02-17 12:22:33 +0300

    третья задача с методом

--- DZseminar2/Zad_3/Program.cs
+++ DZseminar2/Zad_3/Program.cs
@@ -1,13 +1,28 @@
 ﻿//Напишите программу, которая принимает на вход 
 //цифру, обозначающую день недели, и проверяет, 
 //является ли этот день выходным.
-Console.WriteLine("Введите номер дня недели ");
-int numberDay = int.Parse(Console.ReadLine());
-if(numberDay > 5)
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+string numberDay;
+
+Program11(number, out numberDay);
+Console.WriteLine(numberDay);
+
+static void Program11(int d, out string m)
 {
-    Console.WriteLine("Выходной!");
+    if ((d >= 1) && (d <= 7))
+    {
+        if ((d >= 1) && (d <= 5))
+        {
+            m = "Рабочий";
+        }
+        else
+        {
+            m = "ВЫХОДНОЙ!";
+        }
+    }
+    else
+    {
+        m = "Такого дня недели не существует";
+    }
 }
-else
-{
-   Console.WriteLine("Рабочий день"); 
-} 
