c8481068bf5864001c4c57e77088751ada29d30e
Alisa Dhelomanova <89995351029@mail.ru>
2023-02-18 00:24:03 +0300

    решение без метода

---
+++ DZseminar3/Zad_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите координату х для 1 точки: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату y для 1 точки: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату z для 1 точки: ");
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату x для 2 точки: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату y для 2 точки: ");
+int y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату z для 2 точки: ");
+int z2 = int.Parse(Console.ReadLine());
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z2 - z1;
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine(length);
