b9e0bf9c915d8e35c474fc6816f0232c43688787
Alisa Dhelomanova <89995351029@mail.ru>
2023-02-17 19:48:28 +0300

    промежуочный вариант задачи

---
+++ DZseminar3/Zad_3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+int cube;
+if (number >= 1)
+{
+    ProgramCube(number, out cube);
+    Console.WriteLine(cube);
+}
+
+else
+{
+    Console.WriteLine("Ошибка, введите число от 1");
+}
+void ProgramCube(int a, out int i)
+{
+    for (i = 1;i <= a; i++) Console.WriteLine(i * i * i);
+}
