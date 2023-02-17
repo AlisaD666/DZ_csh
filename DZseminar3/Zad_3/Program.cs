ca6dca1717900019e3c9dc086b22004eeed1005d
Alisa Dhelomanova <89995351029@mail.ru>
2023-02-17 23:34:42 +0300

    решена третья задача методом

--- DZseminar3/Zad_3/Program.cs
+++ DZseminar3/Zad_3/Program.cs
@@ -1,17 +1,22 @@
 ﻿Console.WriteLine("Введите число ");
 int number = int.Parse(Console.ReadLine());
 int cube;
+
 if (number >= 1)
 {
-    ProgramCube(number, out cube);
-    Console.WriteLine(cube);
+    ProgramCube(number);
+    Console.WriteLine();
 }
 
 else
 {
     Console.WriteLine("Ошибка, введите число от 1");
 }
-void ProgramCube(int a, out int i)
+void ProgramCube(int a)
 {
-    for (i = 1;i <= a; i++) Console.WriteLine(i * i * i);
+    for (int i = 1;i <= a; i++) 
+    {
+      Console.WriteLine("Число\tКуб");
+      Console.WriteLine("{0}\t{1}", i, Math.Pow(i, 3)); 
+    }
 }
