1d7431dc7fafc06ca83ec206ff9e0bc029d69855
Alisa Dhelomanova <89995351029@mail.ru>
2023-02-18 10:59:07 +0300

    добавила задание

--- DZseminar3/Zad_3/Program.cs
+++ DZseminar3/Zad_3/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите число ");
+﻿//Напишите программу, которая принимает
+//на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число от 1 ");
 int number = int.Parse(Console.ReadLine());
 
 if (number >= 1)
