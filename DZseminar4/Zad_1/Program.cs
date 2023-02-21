abc02235c46dd1fd6f5f6934688d1bc81558d802
Alisa Dhelomanova <89995351029@mail.ru>
2023-02-21 18:54:54 +0300

    первая задача решена

---
+++ DZseminar4/Zad_1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите метод, который принимает на вход два числа (A и B)
+//и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число A ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberB > 0)
+{
+    Console.WriteLine(ProgramDegree(numberA, numberB));
+}
+
+else Console.WriteLine("Введите число B, которое больше 0");
+
+int ProgramDegree(int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i < B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
