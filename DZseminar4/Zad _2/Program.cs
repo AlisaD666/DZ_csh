42798fe9c15c688d24ddd1af5a9859a12c873d56
Alisa Dhelomanova <89995351029@mail.ru>
2023-02-21 19:39:01 +0300

    вторая задача готова

---
+++ DZseminar4/Zad _2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите метод, который принимает на вход
+// число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число  ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр в числе {number} = {SumNumber(number)}");
+
+int SumNumber(int num)
+{
+    int sum = 0;
+while (num > 0)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+}
+return sum;
+}
