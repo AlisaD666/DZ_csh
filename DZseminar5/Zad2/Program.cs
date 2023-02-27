006b8be7fa4c9091158cfa5c1503192c188e500e
Alisa Dhelomanova <89995351029@mail.ru>
2023-02-27 16:37:57 +0300

    вторая задача

---
+++ DZseminar5/Zad2/Program.cs
@@ -0,0 +1,55 @@
+﻿ //Задайте одномерный массив, заполненный случайными числами. 
+ //Найдите сумму элементов, стоящих на нечётных позициях.
+
+ int[] CrateArray(int size)
+{
+    return new int[size];
+}
+
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string Print(int[] array)
+{
+    string result = string.Empty;
+    int len = array.Length;
+    int index = 0;
+    while (index < len)
+    {
+        result = result + " " + array[index];
+        index++;
+    }
+    return result;
+}
+
+void FillByRandom(int[] array)
+{
+    int size = array.Length;
+    int index = 0;
+    Random rand = new Random();
+    while (index < size)
+    {
+        array[index] = rand.Next(1, 10);
+        index++;
+    }
+}
+
+void SumNotEvenNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 1)
+            sum += array[i];
+
+    Console.WriteLine($"сумма нечетных {sum}");
+}
+
+
+int size = GetNumber("Введите размер массива ");
+int[] numbers = CrateArray(size);
+FillByRandom(numbers);
+Console.WriteLine(Print(numbers));
+SumNotEvenNumbers(numbers);
