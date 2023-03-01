ddfd7b69b63bab3bba4f7b0a6c73a762cb3507a5
Alisa Dhelomanova <89995351029@mail.ru>
2023-03-01 13:23:30 +0300

    первая задача

---
+++ DZseminar6/Program.cs
@@ -0,0 +1,56 @@
+﻿
+int [] CrateArray(int size)
+{
+    return new int[size];
+}
+
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string Print(int[] array)
+{
+    string result = string.Empty;
+    int len = array.Length;
+    int index = 0;
+    while(index < len)
+    {
+        result = result + " " + array[index];
+        index++;
+    }
+    return result;
+}
+
+void FillByUser(int[] array)
+{
+   int size = array.Length;
+   int index = 0;
+   while(index < size)
+   {
+        array[index] = GetNumber("Введите " + index + "элемент");
+        index++;
+    }
+}
+
+void CountNumbersPositive(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"{count} чисел больше 0");
+}
+
+
+int size = GetNumber("Введите размер массива ");
+int[] numbers = CrateArray(size);
+Console.WriteLine(Print(numbers));
+FillByUser(numbers);
+Console.WriteLine(Print(numbers));
+CountNumbersPositive(numbers);
