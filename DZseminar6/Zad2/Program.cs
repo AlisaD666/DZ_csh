de4f8fe727653d2c1b04ec946a4c326110989f8e
Alisa Dhelomanova <89995351029@mail.ru>
2023-03-03 14:45:33 +0300

    коррекция

--- DZseminar6/Zad2/Program.cs
+++ DZseminar6/Zad2/Program.cs
@@ -2,22 +2,29 @@
 //заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения
 //b1, k1, b2 и k2 задаются пользователем.
 
-Console.WriteLine("введите значение b1");
-double b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число k1");
-double k1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите значение b2");
-double b2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число k2");
-double k2 = Convert.ToInt32(Console.ReadLine());
-
-void CoordinateIntersection(double a1, double c1, double a2, double c2)
+double GetBumber(string text)
 {
-double X = (-a2 + a1)/(-c1 + c2);
-double Y = c2 * x + a2;
-
-Console.WriteLine($"точка пересечения двух прямых X: {X}, Y: {Y}");
+    Console.WriteLine(text);
+    return Convert.ToDouble(Console.ReadLine());
 }
 
-CoordinateIntersection(b1, k1, b2, k2);
+double StraightLineX(double a1, double c1, double a2, double c2)
+{
+double x = (-a2 + a1)/(-c1 + c2);
+return x;
+}
+
+double StraightLineY(double c2, double a2, double x)
+{
+    double y = c2 * x + a2;
+    return y;
+}
+
+double b1 = GetBumber("Введите b1");
+double k1 = GetBumber("Введите k1");
+double b2 = GetBumber("Введите b2");
+double k2 = GetBumber("Введите k2");
+double X = StraightLineX(b1, k1, b2, k2);
+double Y = StraightLineY(k2, b2, X);
+Console.WriteLine($"точка пересения двух прямых в координатах Х = {X} и Y = {Y}");
 
