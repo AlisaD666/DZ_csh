1ec3c474c5c81a557b7ffac61ac4482db6de6868
Alisa Dhelomanova <89995351029@mail.ru>
2023-03-03 19:14:05 +0300

    третья задача

---
+++ DZseminar7/Zad3/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задайте двумерный массив из целых чисел. Найдите 
+//среднее арифметическое элементов в каждом столбце.
+
+int SizeFromUser(string message)
+{
+    Console.Write(message);
+    int size = Convert.ToInt32(Console.ReadLine());
+    return size;
+}
+
+void PrintArray(int[,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            Console.Write(mass[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void AverageElementColumns(int [,] mass, int colum)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+{
+    double result = 0;
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        result = (result + mass[i, j]);
+    }
+    result = result / colum;
+    Console.Write(result + "  ");
+}
+}
+
+int lines = SizeFromUser("Введите количество строк: ");
+int columns = SizeFromUser("Введите количество столбцов: ");
+int[,] massiv = new int[lines, columns];
+
+FillArray(massiv);
+PrintArray(massiv);
+
+AverageElementColumns(massiv, columns);
