f4599407b06952bd5ade4a1470c68438a1d0bb0f
Alisa Dhelomanova <89995351029@mail.ru>
2023-03-10 19:40:42 +0300

    первые две задачи

---
+++ DZseminar9/Zad1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от N до 1. Выполнить
+// с помощью рекурсии.
+
+int NumberFromUser(string message)
+{
+    Console.Write(message);
+    int numbers = Convert.ToInt32(Console.ReadLine());
+    return numbers;
+}
+
+int NaturalNumbers(int n, int num)
+{
+    if (n == num)
+        return n;
+    else
+        Console.Write($"{NaturalNumbers(n, num + 1)},");
+    return num;
+}
+
+int N = NumberFromUser("Введите число от 1: ");
+int number = 1;
+Console.WriteLine(NaturalNumbers(N, number));
+
+
---
+++ DZseminar9/Zad2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая
+// найдёт сумму натуральных элементов в промежутке от M до N.
+
+int NumberFromUser(string message)
+{
+    Console.Write(message);
+    int numbers = Convert.ToInt32(Console.ReadLine());
+    return numbers;
+}
+
+int SummaMN(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + SummaMN(m, n - 1);
+}
+
+int M = NumberFromUser("Введите число M: ");
+int N = NumberFromUser("Введите число N: ");
+Console.WriteLine(SummaMN(M, N));
